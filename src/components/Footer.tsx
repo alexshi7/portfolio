1d24193b5ff95f786844334309e93ee659810f35
alexcheese7 <a1exh.5hi1@gmail.com>
2024-08-01 00:35:06 -0700

    rebranding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,9 +8,9 @@ function Footer() {
     <footer>
       <div>
         <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-        <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+        <a href="https://www.linkedin.com/in/alex-shi7/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
       </div>
-      <p>A portfolio designed & built by <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer">Yuji Sato</a> with 💜</p>
+      <p>inspired by Yuji</p>
     </footer>
   );
 }
